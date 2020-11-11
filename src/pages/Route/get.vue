1ae509d64b282e4a494a374323b7766ea609223e
Ahmet Sait Akyol <ahmet.akyol@ideateknoloji.com.tr>
2020-11-11 17:57:47 +0300

    Route get ekranı düzeltmeleri.

--- src/pages/Route/get.vue
+++ src/pages/Route/get.vue
@@ -16,9 +16,9 @@
         <b-col cols="12">
           <section>
             <span><i class="fas fa-code" />  <b>{{$t('insert.route.reference')}}:</b> {{rowData.EncryptedKey}}</span> 
-            <span><i class="fas fa-code" />  <b>{{$t('insert.route.status')}}:</b> {{(rowData.StatusId) ? $t('insert.active'): $t('insert.passive')}}</span>
+            <span><i class="fas fa-check" />  <b>{{$t('insert.route.status')}}:</b> {{(rowData.StatusId) ? $t('insert.active'): $t('insert.passive')}}</span>
             <span><i class="fas fa-code" />  <b>{{$t('insert.route.routeCode')}}:</b> {{rowData.Code}}</span>
-            <span><i class="fas fa-code" />  <b>{{$t('insert.route.routeType')}}:</b> {{(rowData.RouteType) ? rowData.RouteType.Label : ''}}</span>
+            <span><i class="far fa-circle" />  <b>{{$t('insert.route.routeType')}}:</b> {{(rowData.RouteType) ? rowData.RouteType.Label : ''}}</span>
           </section>
         </b-col>
         <b-col cols="12" md="4">
@@ -36,7 +36,7 @@
             <p>{{(rowData.Supervisor) ? rowData.Supervisor.Label : ''}}</p>
             <span><i class="far fa-circle" /> {{$t('insert.control')}}</span>
             <p>{{(rowData.VisitStartControl) ? rowData.VisitStartControl.Label : ''}}</p>
-            <span><i class="far fa-circle" /> {{$t('insert.city')}}</span>
+            <!-- <span><i class="far fa-circle" /> {{$t('insert.city')}}</span>
             <p></p>
             <span><i class="far fa-circle" /> {{$t('insert.distirict')}}</span>
             <p></p>
@@ -45,7 +45,7 @@
             <span><i class="far fa-circle" /> {{$t('insert.route.type')}}</span>
             <p></p>
             <span><i class="far fa-circle" /> {{$t('insert.route.customerArea')}}</span>
-            <p></p>
+            <p></p> -->
             <span><i class="far fa-circle" /> {{$t('insert.multiDayRoute')}}</span>
             <p>{{(rowData.IsMultidayRoute) ? $t('insert.yes') : $t('insert.no')}}</p>
             <span><i class="far fa-circle" /> {{$t('insert.superRoute')}}</span>
@@ -54,7 +54,16 @@
         </b-col>
         <b-col cols="12" md="8">
           <b-card class="m-3 asc__showPage-card">
-            <b-table :items="rowData" :fields="fields">
+            <b-table :items="tempItems" :fields="fields">
+              <template #cell(customer)="data">
+                {{data.value}}
+                <br>
+                <br>
+                {{data.item.code}}
+              </template>
+              <template #cell(visit)="data">
+                <i class="fa fa-check text-success"></i>
+              </template>
             </b-table>
           </b-card>
         </b-col>
@@ -68,7 +77,34 @@ export default {
   props: ['dataKey'],
   data () {
     return {
-      fields: ['RepresentativeId', 'EncryptedKey', 'CreatedDateTime']
+      // fields: ['Müşteri', 'Lokasyon', 'Ziyaret Başlama Kontrolü Yapılmayacak'],
+      fields: [
+        {
+          key: 'customer',
+          label: 'Müşteri',
+        },
+        {
+          key: 'location',
+          label: 'Lokasyon',
+        },
+        {
+          key: 'visit',
+          label: 'Ziyaret Başlama Kontrolü Yapılmayacak',
+        }
+      ],
+      tempItems: [
+        { customer: 'CIHAN BUFE- ALI ATAN', location: 'CIHAN BUFE- ALI ATAN', visit: 1, code: '312' },
+        { customer: 'UGUR BUFE', location: 'UGUR BUFE', visit: 1 , code: '260'},
+        { customer: 'ENGIN MARKET-ENGIN SIT', location: 'ENGIN MARKET-ENGIN SIT', visit: 0, code: '312' },
+        { customer: 'MOKKI', location: 'MOKKI', visit: 0 },
+        { customer: 'CAN BÜFE SERKAN ÇALIŞKAN', location: 'CAN BÜFE SERKAN ÇALIŞKAN', visit: 0 , code: '312'},
+        { customer: 'YAVUZ BÜFE', location: 'YAVUZ BÜFE', visit: 0 , code: '312'},
+        { customer: 'MASLAK ARENA', location: 'MASLAK ARENA', visit: 1 , code: '312'},
+        { customer: 'CUMHURİYET BÜFE-MUSTAFA BOZOK', location: 'CUMHURİYET BÜFE-MUSTAFA BOZOK', visit: 1 , code: '312'},
+        { customer: 'TEKEL 61-İDRİS BAŞALI', location: 'TEKEL 61-İDRİS BAŞALI', visit: 1, code: '312' },
+        { customer: 'ARZU GIDA-AHMET AKSU', location: 'ARZU GIDA-AHMET AKSU', visit: 1 , code: '312'},
+        { customer: 'BARBADOS TÜTÜN', location: 'BARBADOS TÜTÜN', visit: 1 , code: '312'},
+      ]
     }
   },
   mounted () {
