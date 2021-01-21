b90a9993b7e7e0b3a62657bd8a44014d4d3956da
Apdurrahman Ezer <apdurrahman.ezer@ideateknoloji.com.tr>
2021-01-21 12:11:03 +0300

    Fixed Term Insert Yeni Yapıya Göre Yapıldı

--- src/pages/FixedTerm/insert.vue
+++ src/pages/FixedTerm/insert.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-row>
+  <b-row class="asc__insertPage">
     <b-col cols="12">
       <header>
         <b-row>
@@ -18,92 +18,145 @@
     <b-col cols="12" class="asc__insertPage-content-head">
       <section>
         <b-row>
-          <b-col cols="12" md="3" lg="2">
-            <b-form-group
-              :label="$t('insert.fixedTerm.code')"
-            >
-              <b-form-input type="text" v-model="form.model.code" readonly />
-            </b-form-group>
-          </b-col>
-          <b-col cols="12" md="3" lg="2">
-            <b-form-group
-              :label="$t('insert.fixedTerm.period')"
-            >
-              <b-form-input type="text" v-model="form.model.period"/>
-            </b-form-group>
-          </b-col>
-          <b-col cols="12" md="3" lg="2">
-            <b-form-group
-              :label="$t('insert.fixedTerm.description1')"
-            >
-              <b-form-input type="text" v-model="form.model.description1"/>
-            </b-form-group>
-          </b-col>
-          <b-col cols="12" md="3" lg="2">
-            <b-form-group
-              :label="$t('insert.status')"
-            >
-              <b-form-checkbox v-model="dataStatus" name="check-button" switch>
-                {{(dataStatus) ? $t('insert.active'): $t('insert.passive')}}
-              </b-form-checkbox>
-            </b-form-group>
-          </b-col>
+                    <b-col v-if="insertVisible.Code != null ? insertVisible.Code : developmentMode" cols="12" md="2">
+              <b-form-group :label="insertTitle.Code + (insertRequired.Code === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.Code.$error }">
+                <b-form-input type="text" v-model="form.Code" :readonly="insertReadonly.Code" />
+              </b-form-group>
+            </b-col>
+                    <b-col v-if="insertVisible.Period != null ? insertVisible.Period : developmentMode" cols="12" md="2">
+              <b-form-group :label="insertTitle.Period + (insertRequired.Period === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.Period.$error }">
+                <b-form-input type="text" v-model="form.Period" :readonly="insertReadonly.Period" />
+              </b-form-group>
+            </b-col>
+                <b-col v-if="insertVisible.Description1 != null ? insertVisible.Description1 : developmentMode" cols="12" md="2">
+              <b-form-group :label="insertTitle.Description1 + (insertRequired.Description1 === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.Description1.$error }">
+                <b-form-input type="text" v-model="form.Description1" :readonly="insertReadonly.Description1" />
+              </b-form-group>
+            </b-col>
+                     <b-col v-if="insertVisible.StatusId != null ? insertVisible.StatusId : developmentMode" cols="12" md="2">
+              <b-form-group :label="insertTitle.StatusId + (insertRequired.StatusId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.StatusId.$error }">
+                <b-form-checkbox v-model="form.StatusId" name="check-button" switch>
+                  {{(form.StatusId) ? $t('insert.active'): $t('insert.passive')}}
+                </b-form-checkbox>
+              </b-form-group>
+            </b-col>
         </b-row>
       </section>
+    </b-col>
+    <b-col cols="12">
+      <b-tabs>
+        <b-tab v-if="developmentMode" :active="developmentMode" title="all inputs">
+          <b-row>
+            <b-col>
+              <pre v-if="developmentMode" class="asc__codeHTML">
+                <span v-for="(codeInCode, i) in insertHTML" :key="'codeInCode' + i">
+                  {{codeInCode}}
+                </span>
+              </pre>
+            </b-col>
+          </b-row>
+          <b-row>
+          </b-row>
+          <b-row>
+            <b-col>
+              <code>{{form}}</code>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="12">
+              <h3>Form Elements</h3>
+              <p>
+                {{insertFormdata}}
+              </p>
+            </b-col>
+          </b-row>
+        </b-tab>
+      </b-tabs>
     </b-col>
   </b-row>
 </template>
 <script>
 import { mapState } from 'vuex'
-
+import mixin from '../../mixins/index'
 export default {
+  mixins: [mixin],
   data () {
     return {
       form: {
-        model: {
-          code: '',
-          description1: '',
-          period: null,
-          statusId: null
-        }
+        Code: null,
+        Period: null,
+        Description1: null
       },
-      dataStatus: null
+      routeName: this.$route.meta.baseLink
     }
   },
   computed: {
-    ...mapState(['createCode'])
+    ...mapState(['developmentMode', 'insertHTML', 'insertDefaultValue', 'insertRules', 'insertRequired', 'insertFormdata', 'insertVisible', 'insertTitle', 'insertReadonly', 'lookup', 'createCode'])
   },
   mounted () {
-    this.$store.commit('bigLoaded', false)
-    this.getCode()
+    this.getInsertPage(this.routeName)
   },
   methods: {
-    getCode () {
-      this.$store.dispatch('getCreateCode', {...this.query, apiUrl: 'VisionNextCommonApi/api/FixedTerm/GetCode'})
+    getInsertPage (e) {
+      // bu fonksiyonda güncelleme yapılmayacak!
+      // her insert ekranının kuralları ve createCode değerini alır.
+      this.$store.dispatch('getInsertRules', {...this.query, api: e})
+      this.$store.dispatch('getCreateCode', {...this.query, apiUrl: `VisionNextCommonApi/api/${e}/GetCode`})
+    },
+    selectedType (label, model) {
+      // bu fonksiyonda güncelleme yapılmayacak!
+      // standart dropdownların select işleminde alacağı değeri belirler.
+      if (model) {
+        this.form[label] = model.DecimalValue
+      } else {
+        this.form[label] = null
+      }
+    },
+    selectedSearchType (label, model) {
+      if (model) {
+        this.form[label] = model.RecordId
+      } else {
+        this.form[label] = null
+      }
     },
     save () {
-      this.$store.dispatch('createData', {...this.query, api: 'VisionNextCommonApi/api/FixedTerm', formdata: this.form, return: this.$route.meta.baseLink})
+      this.$v.$touch()
+      if (this.$v.$error) {
+        this.$store.commit('showAlert', { type: 'danger', msg: this.$t('insert.requiredFields') })
+      } else {
+        let model = {
+          'model': this.form
+        }
+        this.$store.dispatch('createData', {...this.query, api: `VisionNextCommonApi/api/${this.routeName}`, formdata: model, return: this.routeName})
+      }
+    }
+  },
+  validations () {
+    // bu fonksiyonda güncelleme yapılmayacak!
+    // servisten tanımlanmış olan validation kurallarını otomatik olarak içeriye alır.
+    return {
+      form: this.insertRules
     }
   },
   watch: {
-    createCode: function (e) {
+    // bu fonksiyonda güncelleme yapılmayacak!
+    // her insert ekranı sistemden gelen kodla çalışır.
+    createCode (e) {
       if (e) {
-        this.form.model.code = e
-        this.branchCode = Number(e) + 1
+        this.form.Code = e
       }
     },
-    dataStatus: function (e) {
-      if (e === true) {
-        this.form.model.statusId = 1
-      } else {
-        this.form.model.statusId = 0
-      }
+    // bu fonksiyonda güncelleme yapılmayacak!
+    // sistemden gönderilen default değerleri inputlara otomatik basacaktır.
+    insertDefaultValue (value) {
+      Object.keys(value).forEach(el => {
+        if (el !== 'Code') {
+          this.form[el] = value[el]
+        }
+      })
     }
   }
 }
 </script>
-<style lang="sass" scope>
-  table
-    & i
-      cursor: pointer
+<style lang="sass">
 </style>
