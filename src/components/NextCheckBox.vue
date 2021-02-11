24aae3dd714b7d524224d3302f31e7301573ddcb
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2021-02-11 13:02:14 +0300

    checkbox development

---
+++ src/components/NextCheckBox.vue
@@ -0,0 +1,56 @@
+<template>
+    <b-form-checkbox v-model="model" name="check-button" switch>
+      {{model ? textActive : textPassive}}
+    </b-form-checkbox>
+</template>
+<script>
+export default {
+  name: 'NextCheckBox',
+  props: {
+    type: {
+      type: String,
+      validator: (prop) => [
+        'boolean',
+        'number'
+      ].includes(prop),
+      default: 'boolean'
+    },
+    activeText: String,
+    passiveText: String,
+    value: {
+      type: [Boolean, Number]
+    }
+  },
+  model: {
+    prop: 'value',
+    event: 'valuechange'
+  },
+  data () {
+    return {
+      textActive: '',
+      textPassive: '',
+      model: false
+    }
+  },
+  mounted () {
+    this.textActive = this.activeText ? this.activeText : this.$t('insert.active')
+    this.textPassive = this.passiveText ? this.passiveText : this.$t('insert.passive')
+    this.model = this.type === 'boolean' ? this.value : this.value === 1
+  },
+  watch: {
+    model (newValue) {
+      let val = this.type === 'boolean' ? newValue : newValue ? 1 : 0
+      if (val !== this.value) {
+        this.$emit('valuechange', val)
+      }
+    },
+    value (newValue) {
+      let val = this.type === 'boolean' ? newValue : newValue === 1
+      if (val !== this.model) {
+        this.model = val
+      }
+    }
+  }
+}
+
+</script>
