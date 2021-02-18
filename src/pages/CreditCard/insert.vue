200c841815288a43f2714eb752b1b4658dcd3fdd
Mustafa Babayiğit <mustafa.babayigit@ideateknoloji.com.tr>
2021-02-18 09:08:17 +0300

    Credit Card ekranı

--- src/pages/CreditCard/insert.vue
+++ src/pages/CreditCard/insert.vue
@@ -8,9 +8,9 @@
           </b-col>
           <b-col cols="12" md="6" class="text-right">
             <router-link :to="{name: 'Dashboard' }">
-              <b-button size="sm" variant="outline-danger">{{$t('header.cancel')}}</b-button>
+              <CancelButton />
             </router-link>
-            <b-button @click="save()" id="submitButton" size="sm" variant="success">{{$t('header.save')}}</b-button>
+            <AddButton @click.native="save()" />
           </b-col>
         </b-row>
       </header>
@@ -18,7 +18,21 @@
     <b-col cols="12" class="asc__insertPage-content-head">
       <section>
         <b-row>
-        header
+          <b-col v-if="insertVisible.Code != null ? insertVisible.Code : developmentMode" cols="12" md="3">
+            <b-form-group :label="insertTitle.Code + (insertRequired.Code === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.Code.$error }">
+              <b-form-input type="text" v-model="form.Code" :readonly="insertReadonly.Code" />
+            </b-form-group>
+          </b-col>
+          <b-col v-if="insertVisible.Description1 != null ? insertVisible.Description1 : developmentMode" cols="12" md="3">
+            <b-form-group :label="insertTitle.Description1 + (insertRequired.Description1 === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.Description1.$error }">
+              <b-form-input type="text" v-model="form.Description1" :readonly="insertReadonly.Description1" />
+            </b-form-group>
+          </b-col>
+          <b-col v-if="insertVisible.StatusId != null ? insertVisible.StatusId : developmentMode" cols="12" md="2">
+            <b-form-group :label="insertTitle.StatusId + (insertRequired.StatusId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.StatusId.$error }">
+              <NextCheckBox v-model="form.StatusId" type="number" toggle/>
+            </b-form-group>
+          </b-col>
         </b-row>
       </section>
     </b-col>
@@ -26,6 +40,56 @@
       <b-tabs>
         <b-tab :title="$t('firsttab')" :active="!developmentMode">
           <b-row>
+            <b-col v-if="insertVisible.CustomerId != null ? insertVisible.CustomerId : developmentMode" cols="12" md="3">
+              <b-form-group :label="insertTitle.CustomerId + (insertRequired.CustomerId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.CustomerId.$error }">
+                <v-select />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="insertVisible.ApproveNumber != null ? insertVisible.ApproveNumber : developmentMode" cols="12" md="3">
+              <b-form-group :label="insertTitle.ApproveNumber + (insertRequired.ApproveNumber === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.ApproveNumber.$error }">
+                <b-form-input type="text" v-model="form.ApproveNumber" :readonly="insertReadonly.ApproveNumber" />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="insertVisible.DocumentNumber != null ? insertVisible.DocumentNumber : developmentMode" cols="12" md="3">
+              <b-form-group :label="insertTitle.DocumentNumber + (insertRequired.DocumentNumber === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.DocumentNumber.$error }">
+                <b-form-input type="text" v-model="form.DocumentNumber" :readonly="insertReadonly.DocumentNumber" />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="insertVisible.DocumentDate != null ? insertVisible.DocumentDate : developmentMode" :start-weekday="1" cols="12" md="3">
+              <b-form-group :label="insertTitle.DocumentDate + (insertRequired.DocumentDate === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.DocumentDate.$error }">
+                <b-form-datepicker v-model="form.DocumentDate" />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="insertVisible.Bank != null ? insertVisible.Bank : developmentMode" cols="12" md="3">
+              <b-form-group :label="insertTitle.Bank + (insertRequired.Bank === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.Bank.$error }">
+                <v-select />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="insertVisible.CreditCardTotal != null ? insertVisible.CreditCardTotal : developmentMode" cols="12" md="3">
+              <b-form-group :label="insertTitle.CreditCardTotal + (insertRequired.CreditCardTotal === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.CreditCardTotal.$error }">
+                <b-form-input type="text" v-model="form.CreditCardTotal" :readonly="insertReadonly.CreditCardTotal" />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="insertVisible.CurrencyId != null ? insertVisible.CurrencyId : developmentMode" cols="12" md="3">
+              <b-form-group :label="insertTitle.CurrencyId + (insertRequired.CurrencyId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.CurrencyId.$error }">
+                <v-select />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="insertVisible.CardNumber != null ? insertVisible.CardNumber : developmentMode" cols="12" md="3">
+              <b-form-group :label="insertTitle.CardNumber + (insertRequired.CardNumber === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.CardNumber.$error }">
+                <b-form-input type="text" v-model="form.CardNumber" :readonly="insertReadonly.CardNumber" />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="insertVisible.RepresentativeId != null ? insertVisible.RepresentativeId : developmentMode" cols="12" md="3">
+              <b-form-group :label="insertTitle.RepresentativeId + (insertRequired.RepresentativeId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.RepresentativeId.$error }">
+                <v-select />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="insertVisible.RouteId != null ? insertVisible.RouteId : developmentMode" cols="12" md="3">
+              <b-form-group :label="insertTitle.RouteId + (insertRequired.RouteId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.RouteId.$error }">
+                <v-select />
+              </b-form-group>
+            </b-col>
           </b-row>
         </b-tab>
         <b-tab v-if="developmentMode" :active="developmentMode" title="all inputs">
@@ -60,10 +124,31 @@
 </template>
 <script>
 import { mapState } from 'vuex'
+import mixin from '../../mixins/index'
 export default {
+  mixins: [mixin],
   data () {
     return {
-      form: {},
+      form: {
+        Deleted: 0,
+        System: 0,
+        RecordState: 2,
+        StatusId: 1,
+        Code: null,
+        Description1: null,
+        CustomerId: null,
+        ApproveNumber: null,
+        DocumentNumber: null,
+        DocumentDate: null,
+        Bank: null,
+        CreditCardTotal: null,
+        CurrencyId: null,
+        CardNumber: null,
+        RepresentativeId: null,
+        RouteId: null,
+        IsBatcardTransaction: null,
+        SystemCurrencyRate: 0
+      },
       routeName: this.$route.meta.baseLink
     }
   },
@@ -78,19 +163,42 @@ export default {
       // bu fonksiyonda güncelleme yapılmayacak!
       // her insert ekranının kuralları ve createCode değerini alır.
       this.$store.dispatch('getInsertRules', {...this.query, api: e})
-      this.$store.dispatch('getCreateCode', {...this.query, apiUrl: `VisionNext${e}/api/${e}/GetCode`})
+      this.$store.dispatch('getCreateCode', {...this.query, apiUrl: `VisionNextFinance/api/${e}/GetCode`})
     },
     selectedType (label, model) {
       // bu fonksiyonda güncelleme yapılmayacak!
       // standart dropdownların select işleminde alacağı değeri belirler.
-      this.form[label] = model.DecimalValue
+      if (model) {
+        this.form[label] = model.DecimalValue
+      } else {
+        this.form[label] = null
+      }
+    },
+    selectedSearchType (label, model) {
+      if (model) {
+        this.form[label] = model.RecordId
+      } else {
+        this.form[label] = null
+      }
+    },
+    // Tablerin içerisinde eğer validasyon hatası varsa tabların kenarlarının kırmızı olmasını sağlayan fonksiyon
+    tabValidation () {
+      if (this.$v.form.$invalid) {
+        this.$nextTick(() => {
+          this.tabValidationHelper()
+        })
+      }
     },
     save () {
       this.$v.$touch()
       if (this.$v.$error) {
-        this.$store.commit('showAlert', { type: 'error', msg: 'Zorunlu alanları doldurun' })
+        this.$store.commit('showAlert', { type: 'danger', msg: this.$t('insert.requiredFields') })
+        this.tabValidation()
       } else {
-        this.$store.dispatch('createData', {...this.query, api: `VisionNext${this.routeName}/api/${this.routeName}`, formdata: this.form, return: this.routeName})
+        let model = {
+          'model': this.form
+        }
+        this.$store.dispatch('createData', {...this.query, api: `VisionNext${this.routeName}/api/${this.routeName}`, formdata: model, return: this.routeName})
       }
     }
   },
@@ -113,7 +221,9 @@ export default {
     // sistemden gönderilen default değerleri inputlara otomatik basacaktır.
     insertDefaultValue (value) {
       Object.keys(value).forEach(el => {
-        this.form[el] = value[el]
+        if (el !== 'Code') {
+          this.form[el] = value[el]
+        }
       })
     }
   }
