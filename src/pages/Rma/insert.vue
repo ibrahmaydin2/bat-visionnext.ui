f68c0d8342d6e891856662c9d21be1d3481b4cad
mustafa.babayigit <mustafa.babayigit@ideateknoloji.com.tr>
2021-03-02 17:58:48 +0300

    RMA Insert ekranı tamamlandı

--- src/pages/Rma/insert.vue
+++ src/pages/Rma/insert.vue
@@ -18,11 +18,9 @@
     <b-col cols="12" class="asc__insertPage-content-head">
       <section>
         <b-row>
-          <b-col v-if="insertVisible.StatusId != null ? insertVisible.StatusId : developmentMode" cols="12" md="4" lg="3">
-            <b-form-group :label="insertTitle.StatusId + (insertRequired.StatusId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.StatusId.$error }">
-              <NextCheckBox v-model="form.StatusId" type="number" toggle/>
-            </b-form-group>
-          </b-col>
+          <NextFormGroup item-key="StatusId" :error="$v.form.StatusId">
+            <NextCheckBox v-model="form.StatusId" type="number" toggle/>
+          </NextFormGroup>
         </b-row>
       </section>
     </b-col>
@@ -30,133 +28,120 @@
       <b-tabs>
         <b-tab :title="$t('firsttab')" :active="!developmentMode">
           <b-row>
-            <b-col v-if="insertVisible.CustomerId != null ? insertVisible.CustomerId : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.CustomerId + (insertRequired.CustomerId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.CustomerId.$error }">
-                <v-select :options="customers"  @search="searchCustomer" @input="selectedSearchType('CustomerId', $event)" label="Description1">
-                  <template slot="no-options">
-                    {{$t('insert.min3')}}
-                  </template>
-                </v-select>
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.WarehouseId != null ? insertVisible.WarehouseId : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.WarehouseId + (insertRequired.WarehouseId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.WarehouseId.$error }">
-                <v-select :options="warehouses"  @search="searchWarehouse" @input="selectedSearchType('WarehouseId', $event)" label="Description1">
-                  <template slot="no-options">
-                    {{$t('insert.min3')}}
-                  </template>
-                </v-select>
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.RepresentativeId != null ? insertVisible.RepresentativeId : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.RepresentativeId + (insertRequired.RepresentativeId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.RepresentativeId.$error }">
-                <v-select :options="employees"  @search="searchEmployee" @input="selectedSearchType('RepresentativeId', $event)" label="Description1">
-                  <template slot="no-options">
-                    {{$t('insert.min3')}}
-                  </template>
-                </v-select>
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.RmaStatusId != null ? insertVisible.RmaStatusId : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.RmaStatusId + (insertRequired.RmaStatusId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.RmaStatusId.$error }">
-                <v-select
-                  :options="lookup.RMA_STATUS"
-                  @input="selectedType('RmaStatusId', $event)"
-                  label="Label"
-                />
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.ApproveEmployeeId != null ? insertVisible.ApproveEmployeeId : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.ApproveEmployeeId + (insertRequired.ApproveEmployeeId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.ApproveEmployeeId.$error }">
-                <v-select :options="employees"  @search="searchEmployee" @input="selectedSearchType('ApproveEmployeeId', $event)" label="Description1">
-                  <template slot="no-options">
-                    {{$t('insert.min3')}}
-                  </template>
-                </v-select>
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.ApproveNumber != null ? insertVisible.ApproveNumber : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.ApproveNumber + (insertRequired.ApproveNumber === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.ApproveNumber.$error }">
-                <b-form-input type="text" v-model="form.ApproveNumber" :readonly="insertReadonly.ApproveNumber" />
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.RmaTypeId != null ? insertVisible.RmaTypeId : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.RmaTypeId + (insertRequired.RmaTypeId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.RmaTypeId.$error }">
-                <v-select
-                  :options="lookup.RETURN_TYPE"
-                  @input="selectedType('RmaTypeId', $event)"
-                  label="Label"
-                />
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.ApproveDate != null ? insertVisible.ApproveDate : developmentMode" :start-weekday="1" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.ApproveDate + (insertRequired.ApproveDate === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.ApproveDate.$error }">
-                <b-form-datepicker v-model="form.ApproveDate" />
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.PriceDate != null ? insertVisible.PriceDate : developmentMode" :start-weekday="1" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.PriceDate + (insertRequired.PriceDate === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.PriceDate.$error }">
-                <b-form-datepicker v-model="form.PriceDate" />
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.Genexp1 != null ? insertVisible.Genexp1 : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.Genexp1 + (insertRequired.Genexp1 === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.Genexp1.$error }">
-                <b-form-input type="text" v-model="form.Genexp1" :readonly="insertReadonly.Genexp1" />
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.RmaDate != null ? insertVisible.RmaDate : developmentMode" :start-weekday="1" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.RmaDate + (insertRequired.RmaDate === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.RmaDate.$error }">
-                <b-form-datepicker v-model="form.RmaDate" />
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.GrvNumber != null ? insertVisible.GrvNumber : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.GrvNumber + (insertRequired.GrvNumber === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.GrvNumber.$error }">
-                <b-form-input type="text" v-model="form.GrvNumber" :readonly="insertReadonly.GrvNumber" />
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.RouteId != null ? insertVisible.RouteId : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.RouteId + (insertRequired.RouteId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.RouteId.$error }">
-                <v-select :options="routes"  @search="searchRoute" @input="selectedSearchType('RouteId', $event)" label="Description1">
-                  <template slot="no-options">
-                    {{$t('insert.min3')}}
-                  </template>
-                </v-select>
-              </b-form-group>
-            </b-col>
-            <b-col v-if="insertVisible.RmaReasonId != null ? insertVisible.RmaReasonId : developmentMode" cols="12" md="4" lg="3">
-              <b-form-group :label="insertTitle.RmaReasonId + (insertRequired.RmaReasonId === true ? ' *' : '')" :class="{ 'form-group--error': $v.form.RmaReasonId.$error }">
-                <v-select :options="rmaReasons"  @search="searchRmaReason" @input="selectedSearchType('RmaReasonId', $event)" label="Description1">
-                  <template slot="no-options">
-                    {{$t('insert.min3')}}
-                  </template>
-                </v-select>
-              </b-form-group>
-            </b-col>
+            <NextFormGroup item-key="CustomerId" :error="$v.form.CustomerId">
+              <v-select :options="customers"  @search="searchCustomer" @input="selectedSearchType('CustomerId', $event)" label="Description1">
+                <template slot="no-options">
+                  {{$t('insert.min3')}}
+                </template>
+              </v-select>
+            </NextFormGroup>
+            <NextFormGroup item-key="WarehouseId" :error="$v.form.WarehouseId">
+              <v-select :options="warehouses"  @search="searchWarehouse" @input="selectedSearchType('WarehouseId', $event)" label="Description1">
+                <template slot="no-options">
+                  {{$t('insert.min3')}}
+                </template>
+              </v-select>
+            </NextFormGroup>
+            <NextFormGroup item-key="RepresentativeId" :error="$v.form.RepresentativeId">
+              <v-select :options="employees"  @search="searchEmployee" @input="selectedSearchType('RepresentativeId', $event)" label="Description1">
+                <template slot="no-options">
+                  {{$t('insert.min3')}}
+                </template>
+              </v-select>
+            </NextFormGroup>
+            <NextFormGroup item-key="RmaStatusId" :error="$v.form.RmaStatusId">
+              <v-select
+                :options="lookup.RMA_STATUS"
+                @input="selectedType('RmaStatusId', $event)"
+                label="Label"
+              />
+            </NextFormGroup>
+            <NextFormGroup item-key="ApproveEmployeeId" :error="$v.form.ApproveEmployeeId">
+              <v-select :options="employees"  @search="searchEmployee" @input="selectedSearchType('ApproveEmployeeId', $event)" label="Description1">
+                <template slot="no-options">
+                  {{$t('insert.min3')}}
+                </template>
+              </v-select>
+            </NextFormGroup>
+            <NextFormGroup item-key="ApproveNumber" :error="$v.form.ApproveNumber">
+              <b-form-input type="text" v-model="form.ApproveNumber" :readonly="insertReadonly.ApproveNumber" />
+            </NextFormGroup>
+            <NextFormGroup item-key="RmaTypeId" :error="$v.form.RmaTypeId">
+              <v-select
+                :options="lookup.RETURN_TYPE"
+                @input="selectedType('RmaTypeId', $event)"
+                label="Label"
+              />
+            </NextFormGroup>
+            <NextFormGroup item-key="ApproveDate" :error="$v.form.ApproveDate">
+              <b-form-datepicker v-model="form.ApproveDate" />
+            </NextFormGroup>
+            <NextFormGroup item-key="PriceDate" :error="$v.form.PriceDate">
+              <b-form-datepicker v-model="form.PriceDate" />
+            </NextFormGroup>
+            <NextFormGroup item-key="Genexp1" :error="$v.form.Genexp1">
+              <b-form-input type="text" v-model="form.Genexp1" :readonly="insertReadonly.Genexp1" />
+            </NextFormGroup>
+            <NextFormGroup item-key="RmaDate" :error="$v.form.RmaDate">
+              <b-form-datepicker v-model="form.RmaDate" />
+            </NextFormGroup>
+            <NextFormGroup item-key="GrvNumber" :error="$v.form.GrvNumber">
+              <b-form-input type="text" v-model="form.GrvNumber" :readonly="insertReadonly.GrvNumber" />
+            </NextFormGroup>
+            <NextFormGroup item-key="RouteId" :error="$v.form.RouteId">
+              <v-select :options="routes"  @search="searchRoute" @input="selectedSearchType('RouteId', $event)" label="Description1">
+                <template slot="no-options">
+                  {{$t('insert.min3')}}
+                </template>
+              </v-select>
+            </NextFormGroup>
+            <NextFormGroup item-key="RmaReasonId" :error="$v.form.RmaReasonId">
+              <v-select :options="rmaReasons"  @search="searchRmaReason" @input="selectedSearchType('RmaReasonId', $event)" label="Description1">
+                <template slot="no-options">
+                  {{$t('insert.min3')}}
+                </template>
+              </v-select>
+            </NextFormGroup>
           </b-row>
-        </b-tab>
-        <b-tab v-if="developmentMode" :active="developmentMode" title="all inputs">
+          <hr />
           <b-row>
-            <b-col>
-              <pre v-if="developmentMode" class="asc__codeHTML">
-                <span v-for="(codeInCode, i) in insertHTML" :key="'codeInCode' + i">
-                  {{codeInCode}}
-                </span>
-              </pre>
-            </b-col>
-          </b-row>
-          <b-row>
-          </b-row>
-          <b-row>
-            <b-col>
-              <code>{{form}}</code>
+            <NextFormGroup :title="$t('insert.RMA.ItemName')">
+              <v-select :options="items" v-model="rmaLine.Item.Description1" @search="searchItem" @input="selectedItem" label="Description1">
+                <template slot="no-options">
+                  {{$t('insert.min3')}}
+                </template>
+              </v-select>
+            </NextFormGroup>
+            <NextFormGroup :title="$t('insert.RMA.Item')">
+              <b-form-input type="text" v-model="rmaLine.Item.Code" readonly/>
+            </NextFormGroup>
+            <NextFormGroup :title="$t('insert.RMA.Quantity')" :error="$v.rmaLine.Quantity">
+              <b-form-input type="text" v-model="rmaLine.Quantity"/>
+            </NextFormGroup>
+            <b-col md="1" class="ml-auto">
+              <b-form-group>
+                <b-button @click="addItems()" class="mt-4" variant="success" size="sm"><i class="fa fa-plus"></i>{{$t('insert.add')}}</b-button>
+              </b-form-group>
             </b-col>
           </b-row>
           <b-row>
             <b-col cols="12">
-              <h3>Form Elements</h3>
-              <p>
-                {{insertFormdata}}
-              </p>
+              <b-table-simple responsive bordered small>
+                <b-thead>
+                  <b-th><span>{{$t('insert.RMA.ItemName')}}</span></b-th>
+                  <b-th><span>{{$t('insert.RMA.Item')}}</span></b-th>
+                  <b-th><span>{{$t('insert.RMA.Quantity')}}</span></b-th>
+                  <b-th><span>{{$t('list.operations')}}</span></b-th>
+                </b-thead>
+                <b-tbody>
+                  <b-tr v-for="(w, i) in rmaLines" :key="i">
+                    <b-td>{{w.Item.Description1}}</b-td>
+                    <b-td>{{w.Item.Code}}</b-td>
+                    <b-td>{{w.Quantity}}</b-td>
+                    <b-td class="text-center"><i @click="removeItems(w)" class="far fa-trash-alt text-danger"></i></b-td>
+                  </b-tr>
+                </b-tbody>
+              </b-table-simple>
             </b-col>
           </b-row>
         </b-tab>
@@ -166,6 +151,7 @@
 </template>
 <script>
 import { mapState } from 'vuex'
+import { required } from 'vuelidate/lib/validators'
 import insertMixin from '../../mixins/insert'
 export default {
   mixins: [insertMixin],
@@ -192,7 +178,7 @@ export default {
         RmaReasonId: null,
         RmaLines: []
       },
-      RmaLines: {
+      rmaLine: {
         Deleted: 0,
         System: 0,
         RecordState: 2,
@@ -207,19 +193,22 @@ export default {
         Quantity: null,
         RmaQuantity1: null,
         RmaUnit1Id: null,
-        Price: null
-      }
+        Price: null,
+        Item: {
+          Description1: null,
+          Code: null,
+          RecordId: null
+        }
+      },
+      rmaLines: [],
+      routeName1: 'Rma'
     }
   },
   computed: {
-    // search items gibi yapılarda state e maplemek için kullanılır. İhtiyaç yoksa silinebilir.
-    ...mapState(['customers', 'employees', 'warehouses', 'routes', 'rmaReasons'])
+    ...mapState(['customers', 'employees', 'warehouses', 'routes', 'rmaReasons', 'items'])
   },
   mounted () {
-    this.createManualCode()
-    // update işlemiyse
-    // this.getData().then(() => {})
-    this.getInsertPage(this.routeName)
+    // this.createManualCode()
   },
   methods: {
     searchEmployee (search, loading) {
@@ -282,6 +271,85 @@ export default {
         loading(false)
       })
     },
+    searchItem (search, loading) {
+      if (search.length < 3) {
+        return false
+      }
+      loading(true)
+      let model = {
+        Description1: search
+      }
+      this.searchItemsByModel('VisionNextItem/api/Item/Search', 'items', model).then(res => {
+        loading(false)
+      })
+    },
+    addItems () {
+      this.$v.rmaLine.$touch()
+      if (this.$v.rmaLine.$error) {
+        this.$toasted.show(this.$t('insert.requiredFields'), {
+          type: 'error',
+          keepOnHover: true,
+          duration: '3000'
+        })
+        return false
+      }
+      let filteredArr = this.rmaLines.filter(i => i.Item.RecordId === this.rmaLine.Item.RecordId)
+      if (filteredArr.length > 0) {
+        this.$store.commit('showAlert', { type: 'danger', msg: this.$t('insert.sameItemError') })
+        return false
+      }
+      if (this.rmaLines.length > 0) {
+        this.rmaLine.LineNumber = (this.rmaLines[(this.rmaLines.length) - 1].LineNumber) + 1
+      }
+      this.initRmaLine(this.rmaLine.Item)
+      this.rmaLines.push(this.rmaLine)
+      this.initNullRmaLine()
+      this.$v.rmaLine.$reset()
+    },
+    removeItems (item) {
+      this.rmaLines.splice(this.rmaLines.indexOf(item), 1)
+    },
+    initRmaLine (value) {
+      this.rmaLine.UnitSetId = value.UnitSetId
+      this.rmaLine.UnitId = value.UnitId
+      this.rmaLine.RmaQuantity1 = this.rmaLine.Quantity
+      this.rmaLine.RmaUnit1Id = this.rmaLine.UnitId
+    },
+    initNullRmaLine () {
+      this.rmaLine = {
+        Deleted: 0,
+        System: 0,
+        RecordState: 2,
+        StatusId: 1,
+        LineNumber: 0,
+        ItemId: null,
+        RmaReasonId: null,
+        UnitSetId: null,
+        UnitId: null,
+        ConvFact1: 1,
+        ConvFact2: 1,
+        Quantity: null,
+        RmaQuantity1: null,
+        RmaUnit1Id: null,
+        Price: null,
+        Item: {
+          Description1: null,
+          Code: null,
+          RecordId: null
+        }
+      }
+    },
+    selectedItem (e) {
+      if (e) {
+        this.rmaLine.Item = e
+      } else {
+        this.rmaLine.Item = {
+          Description1: null,
+          Code: null,
+          RecordId: null
+        }
+      }
+    },
     save () {
       this.$v.form.$touch()
       if (this.$v.form.$error) {
@@ -292,6 +360,8 @@ export default {
         })
         this.tabValidation()
       } else {
+        this.form.RmaLines = this.rmaLines
+        console.log(this.form)
         this.createData()
         // update işlemiyse
         // this.updateData()
@@ -301,7 +371,12 @@ export default {
   validations () {
     // Eğer Detay Panelde validasyon yapılacaksa kullanılmalı. Detay Panel yoksa silinebilir.
     return {
-      form: this.insertRules
+      form: this.insertRules,
+      rmaLine: {
+        Quantity: {
+          required
+        }
+      }
     }
   }
 }
