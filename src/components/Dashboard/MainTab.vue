2b358108f8f0d1a330d6a37123f8904545926d9d
Ahmet Sait Akyol <ahmet.akyol@ideateknoloji.com.tr>
2021-03-04 17:32:45 +0300

    Dashboard'a başlandı. Devam edilecek.

---
+++ src/components/Dashboard/MainTab.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-tabs content-class="mt-3">
+    <b-tab title="First" active><p>I'm the first tab</p></b-tab>
+    <b-tab title="Second"><p>I'm the second tab</p></b-tab>
+    <b-tab title="Disabled"><p>I'm a disabled tab!</p></b-tab>
+  </b-tabs>
+</template>
+<script>
+export default {
+  name: 'MainTab'
+}
+
+</script>
