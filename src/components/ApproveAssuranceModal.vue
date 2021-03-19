5f92b536d523ccc2405262807831db0d0a7b100a
Mustafa Babayiğit <mustafa.babayigit@ideateknoloji.com.tr>
2021-03-19 17:03:45 +0300

    PriceDiscountTransaction güncellemesi, ApproveAssurance componenti, Discount düzenlemesi

---
+++ src/components/ApproveAssuranceModal.vue
@@ -0,0 +1,157 @@
+<template>
+  <b-row>
+    <b-col cols="12">
+      <section>
+        <b-row>
+            <NextFormGroup :title="$t('insert.approveAssuranceModal.customerName')" :lg="4">
+              <v-select :options="customers"  @search="searchCustomer" @input="selectedSearchType('CustomerId', $event)" label="Description1">
+                <template slot="no-options">
+                  {{$t('insert.min3')}}
+                </template>
+              </v-select>
+            </NextFormGroup>
+            <NextFormGroup :title="$t('insert.approveAssuranceModal.customerCode')" :lg="4">
+              <b-form-input type="text" v-model="CustomerCode" disabled />
+            </NextFormGroup>
+            <NextFormGroup :title="$t('insert.approveAssuranceModal.customerArea')" :lg="4">
+              <v-select :options="routeTypeOptions" @input="selectedCustomerRegion" label="Label"></v-select>
+            </NextFormGroup>
+            <NextFormGroup :title="$t('insert.approveAssuranceModal.route')" :lg="4">
+              <v-select :options="routes"  @search="searchRoute" @input="selectedSearchType('RoueteId', $event)" label="Description1">
+                <template slot="no-options">
+                  {{$t('insert.min3')}}
+                </template>
+              </v-select>
+            </NextFormGroup>
+            <NextFormGroup :title="$t('insert.approveAssuranceModal.approveStateId')" :lg="4">
+                <v-select
+                  v-model="selectedApproveState"
+                  :options="lookup.APPROVE_STATE"
+                  @input="selectedType('ApproveStateId', $event)"
+                  label="Label"
+                />
+            </NextFormGroup>
+            <b-col>
+                <b-button size="sm" class="float-right ml-2" variant="success">{{$t('insert.okay')}}</b-button>
+                <b-button size="sm" class="float-right ml-2" variant="outline-danger">{{$t('insert.clean')}}</b-button>
+                <b-button size="sm" class="float-right ml-2" variant="success">{{$t('insert.find')}}</b-button>
+            </b-col>
+        </b-row>
+      </section>
+    </b-col>
+         <b-table
+          :items="approveAssurances"
+          :fields="approveAssuranceFields"
+          select-mode="multi"
+          responsive
+          id="approveAssurance-list"
+          :selectable="true"
+          bordered
+          tbody-tr-class="bg-white"
+          @row-selected="onApproveAssuranceSelected"
+        >
+        <template #cell(selection)="row">
+          <span>
+            <i :class="row.rowSelected ? 'fa fa-check-circle success-color' : 'fa fa-check-circle gray-color'"></i>
+          </span>
+        </template>
+      </b-table>
+      <b-pagination
+        :total-rows="approveAssurances ? approveAssurances.length : 0"
+        v-model="currentPage"
+        :per-page="10"
+        aria-controls="approveAssurance-list"
+      ></b-pagination>
+  </b-row>
+</template>
+<script>
+import { mapState } from 'vuex'
+import insertMixin from '../mixins/insert'
+export default {
+  mixins: [insertMixin],
+  props: ['dataId'],
+  data () {
+    return {
+      form: {
+        CustomerId: null,
+        customerRegion5Id: null,
+        RouteId: null,
+        ApproveStateId: null
+      },
+      CustomerCode: null,
+      selectedApproveState: null,
+      approveAssurances: [],
+      selectedApproveAssurances: [],
+      approveAssuranceFields: [
+        {key: 'selection', label: '', sortable: false, visibility: true},
+        {key: 'form.CustomerId', label: this.$t('insert.approveAssuranceModal.customerName'), sortable: false},
+        {key: 'CustomerCode', label: this.$t('insert.approveAssuranceModal.customerCode'), sortable: false},
+        {key: 'form.CustomerArea', label: this.$t('insert.approveAssuranceModal.customerArea'), sortable: false},
+        {key: 'form.RouteId', label: this.$t('insert.approveAssuranceModal.route'), sortable: false},
+        {key: 'form.ApproveStateId', label: this.$t('insert.approveAssuranceModal.approveStateId'), sortable: false}
+      ]
+    }
+  },
+  mounted () {
+    this.getData()
+  },
+  computed: {
+    ...mapState(['customers', 'routes', 'routeTypeOptions'])
+  },
+  methods: {
+    getData () {
+      let payload = {
+        name: 'routeTypeOptions',
+        data: []
+      }
+      this.$store.dispatch('getLookups', {...this.query, type: 'CUSTOMER_REGION_5', name: payload.name})
+    },
+    searchCustomer (search, loading) {
+      if (search.length < 3) {
+        return false
+      }
+      loading(true)
+      let model = {
+        Description1: search
+      }
+      this.searchItemsByModel('VisionNextCustomer/api/Customer/Search', 'customers', model).then(res => {
+        loading(false)
+      })
+    },
+    searchRoute (search, loading) {
+      if (search.length < 3) {
+        return false
+      }
+      loading(true)
+      let model = {
+        Description1: search
+      }
+      this.searchItemsByModel('VisionNextRoute/api/Route/Search', 'routes', model).then(res => {
+        loading(false)
+      })
+    },
+    selectedCustomerRegion (e) {
+      if (e) {
+        this.form.customerRegion5Id = e.DecimalValue
+      } else {
+        this.form.customerRegion5Id = null
+      }
+    },
+    selectedSearchType (label, model) {
+      if (model) {
+        if (label === 'CustomerId') {
+          this.CustomerCode = model.Code
+        }
+        this.form[label] = model.RecordId
+      } else {
+        this.form[label] = null
+      }
+    },
+    onApproveAssuranceSelected (items) {
+      this.selectedApproveAssurances = items
+    }
+  },
+  watch: {
+  }
+}
+</script>
--- src/pages/PriceDiscountTransaction/get.vue
+++ src/pages/PriceDiscountTransaction/get.vue
@@ -3,143 +3,86 @@
     <div class="asc__showPage-times">
       <i class="fas fa-times-circle" @click="closeQuick()" />
     </div>
-    <div v-if="rowData" class="asc__showPage-container">
+    <div class="asc__showPage-container">
       <b-row>
         <b-col cols="12">
           <header>
-            <Breadcrumb :title="rowData.Description1" />
-            <div class="clearfix"></div>
+            <Breadcrumb />
           </header>
         </b-col>
       </b-row>
       <b-row>
         <b-col cols="12">
           <section>
-            <span><i class="fas fa-check" />  <b>{{$t('insert.warehouse.status')}}:</b> {{(rowData.Status) ? rowData.Status.Label : ''}}</span>
-            <span><i class="fas fa-code" />  <b>{{$t('insert.warehouse.code')}}:</b> {{rowData.Code}}</span>
+            <span><i class="fas fa-code" />  <b>{{$t('get.PriceDiscountTransaction.Code')}}:</b> {{rowData.Code}}</span>
+            <span><i class="fas fa-code" />  <b>{{$t('get.PriceDiscountTransaction.Description')}}:</b> {{rowData.Description1}}</span>
+            <span><i class="fas fa-check" />  <b>{{$t('get.PriceDiscountTransaction.Status')}}:</b> {{(rowData.StatusId) ? $t('insert.active') : $t('insert.passive')}}</span>
           </section>
         </b-col>
       </b-row>
       <b-tabs>
-        <b-tab :title="$t('insert.warehouse.Model_WarehouseTypeId')" active>
-          <b-row>
-            <b-col cols="12" md="4">
-              <b-card class="m-3 asc__showPage-card">
-                <h6>{{$t('insert.warehouse.title')}}</h6>
-                <span><i class="far fa-circle" /> {{$t('insert.warehouse.Model_WarehouseTypeId')}}</span> <p>{{rowData.WarehouseType ? rowData.WarehouseType.Label : ''}}</p>
-                <div v-if="rowData.WarehouseTypeId === 76506193">
-                  <span><i class="far fa-circle" /> {{$t('insert.warehouse.VehicleId')}}</span> <p>{{rowData.Vehicle ? rowData.Vehicle.Label : ''}}</p>
-                </div>
-                <div v-if="rowData.WarehouseTypeId === 76506191">
-                  <span><i class="far fa-circle" /> {{$t('insert.warehouse.Customer')}}</span> <p>{{rowData.Customer ? rowData.Customer.Label : ''}}</p>
-                </div>
-                <span><i class="far fa-circle" /> {{$t('insert.warehouse.IsCenterWarehouse')}}</span> <p><i :class="rowData.IsCenterWarehouse === 1 ? 'fa fa-check text-success' : 'fa fa-times text-danger'"></i></p>
-                <span><i class="far fa-circle" /> {{$t('insert.warehouse.WarehouseCapacity')}}</span> <p>{{rowData.WarehouseCapacity}}</p>
-                <span><i class="far fa-circle" /> {{$t('insert.warehouse.LicenseNumber')}}</span> <p>{{rowData.LicenseNumber}}</p>
-                <span><i class="far fa-circle" /> {{$t('insert.warehouse.FinanceCode')}}</span> <p>{{rowData.FinanceCode}}</p>
-              </b-card>
-            </b-col>
-            <b-col cols="12" md="8">
-              <b-card class="m-3 asc__showPage-card">
-                <h6>{{$t('insert.warehouse.locations')}}</h6>
-                <b-table responsive :items="rowData.WarehouseSuppliers" :fields="fields">
-                  <template #cell(SupplierBranchId)="data">
-                    {{data.item.SupplierBranch.Label}}
-                  </template>
-                  <template #cell(PurchaseWarehouseId)="data">
-                    {{data.item.PurchaseWarehouse.Label}}
-                  </template>
-                  <template #cell(ReturnWarehouseId)="data">
-                    {{data.item.ReturnWarehouse.Label}}
-                  </template>
-                </b-table>
-              </b-card>
-            </b-col>
+        <b-tab :title="$t('get.RmaOrder.RmaOrder')" active>
+          <b-row class="p-4">
+            <b-card class="col-md-6 col-12 asc__showPage-card">
+              <div v-html="getFormatDataByType(rowData.TciBreak1, 'object', 'get.PriceDiscountTransaction.TciBreak1')"></div>
+              <div v-html="getFormatDataByType(rowData.TransactionDate, 'date', 'get.PriceDiscountTransaction.TransactionDate')"></div>
+              <div v-html="getFormatDataByType(rowData.TransactionTime, 'text', 'get.PriceDiscountTransaction.TransactionTime')"></div>
+              <div v-html="getFormatDataByType(rowData.DiscountReason, 'object', 'get.PriceDiscountTransaction.DiscountReason')"></div>
+              <div v-html="getFormatDataByType(rowData.PriceDiscountAmount, 'text', 'get.PriceDiscountTransaction.PriceDiscountAmount')"></div>
+              <div v-html="getFormatDataByType(rowData.Currency, 'object', 'get.PriceDiscountTransaction.Currency')"></div>
+              <div v-html="getFormatDataByType(rowData.BudgetAmount, 'text', 'get.PriceDiscountTransaction.BudgetAmount')"></div>
+              <div v-html="getFormatDataByType(rowData.BudgetConsumption, 'text', 'get.PriceDiscountTransaction.BudgetConsumption')"></div>
+            </b-card>
+            <b-card class="col-md-6 col-12 asc__showPage-card">
+              <div v-html="getFormatDataByType(rowData.Budget, 'object', 'get.PriceDiscountTransaction.Budget')"></div>
+              <div v-html="getFormatDataByType(rowData.ApproveState, 'object', 'get.PriceDiscountTransaction.ApproveState')"></div>
+              <div v-html="getFormatDataByType(rowData.ErpCode, 'text', 'get.PriceDiscountTransaction.ErpCode')"></div>
+              <div v-html="getFormatDataByType(rowData.ExpirationDate, 'date', 'get.PriceDiscountTransaction.ExpirationDate')"></div>
+              <div v-html="getFormatDataByType(rowData.BeginDate, 'date', 'get.PriceDiscountTransaction.BeginDate')"></div>
+              <div v-html="getFormatDataByType(rowData.Genexp1, 'text', 'get.PriceDiscountTransaction.Genexp1')"></div>
+              <div v-html="getFormatDataByType(rowData.GainType, 'object', 'get.PriceDiscountTransaction.GainType')"></div>
+            </b-card>
           </b-row>
         </b-tab>
-        <b-tab v-if="false" :title="$t('insert.other')">
-          <b-row>
-            <b-col cols="12" md="4">
-              <b-card class="m-3 asc__showPage-card">
-                <h6>{{$t('insert.route.other')}}</h6>
-                <span><i class="far fa-circle" /> BranchId </span>
-                <p>{{ rowData.BranchId }}</p>
-                <span><i class="far fa-circle" /> CompanyId </span>
-                <p>{{ rowData.CompanyId }}</p>
-                <span><i class="far fa-circle" /> CreatedDateTime </span>
-                <p>{{ rowData.CreatedDateTime }}</p>
-                <span><i class="far fa-circle" /> CreatedUser </span>
-                <p>{{ rowData.CreatedUser }}</p>
-                <span><i class="far fa-circle" /> CustomerId </span>
-                <p>{{ rowData.CustomerId }}</p>
-                <span><i class="far fa-circle" /> EncryptedKey </span>
-                <p>{{ rowData.EncryptedKey }}</p>
-                <span><i class="far fa-circle" /> IsCustomerWarehouse </span>
-                <p>{{ rowData.IsCustomerWarehouse }}</p>
-                <span><i class="far fa-circle" /> IsVehicle </span>
-                <p>{{ rowData.IsVehicle }}</p>
-                <span><i class="far fa-circle" /> LocationId </span>
-                <p>{{ rowData.LocationId }}</p>
-                <span><i class="far fa-circle" /> ModifiedDateTime </span>
-                <p>{{ rowData.ModifiedDateTime }}</p>
-                <span><i class="far fa-circle" /> ModifiedUser </span>
-                <p>{{ rowData.ModifiedUser }}</p>
-                <span><i class="far fa-circle" /> RecordId </span>
-                <p>{{ rowData.RecordId }}</p>
-                <span><i class="far fa-circle" /> RecordState </span>
-                <p>{{ rowData.RecordState }}</p>
-                <span><i class="far fa-circle" /> RecordStatus </span>
-                <p>{{ rowData.RecordStatus }}</p>
-                <span><i class="far fa-circle" /> System </span>
-                <p>{{ rowData.System }}</p>
-                <span><i class="far fa-circle" /> WarehouseStocks </span>
-                <p>{{ rowData.WarehouseStocks }}</p>
-                <span><i class="far fa-circle" /> WarehouseSuppliers </span>
-                <p>{{ rowData.WarehouseSuppliers }}</p>
-              </b-card>
-            </b-col>
-           </b-row>
-         </b-tab>
       </b-tabs>
     </div>
   </div>
 </template>
 <script>
 import { mapState } from 'vuex'
+import mixin from '../../mixins/index'
 export default {
+  mixins: [mixin],
   props: ['dataKey'],
   data () {
     return {
-      // fields: ['Müşteri', 'Lokasyon', 'Ziyaret Başlama Kontrolü Yapılmayacak'],
-      fields: [
-        {key: 'SupplierBranchId', label: 'Şube', sortable: true},
-        {key: 'PurchaseWarehouseId', label: 'Satın Alma Deposu', sortable: true},
-        {key: 'ReturnWarehouseId', label: 'İade Deposu', sortable: true}
-      ],
-      tempItems: [
-        {SupplierBranchId: '', PurchaseWarehouseId: '', ReturnWarehouseId: ''},
-        {SupplierBranchId: '', PurchaseWarehouseId: '', ReturnWarehouseId: ''},
-        {SupplierBranchId: '', PurchaseWarehouseId: '', ReturnWarehouseId: ''},
-        {SupplierBranchId: '', PurchaseWarehouseId: '', ReturnWarehouseId: ''}
-      ]
+      RmaStatus: {},
+      RmaType: {}
     }
   },
   mounted () {
     this.getData()
     this.$store.commit('bigLoaded', false)
+    let allLookups = 'RMA_STATUS,RETURN_TYPE'
+    this.$store.dispatch('getAllLookups', {...this.query, type: allLookups})
   },
   computed: {
-    ...mapState(['rowData', 'style'])
+    ...mapState(['rowData', 'style', 'lookup'])
   },
   methods: {
     closeQuick () {
       this.$router.push({name: this.$route.meta.base})
     },
     getData () {
-      this.$store.dispatch('getData', {...this.query, api: 'VisionNextDiscount/api/PriceDiscountTransaction', record: this.$route.params.url})
+      this.$store.dispatch('getData', {...this.query, api: 'VisionNextDiscount/api/PriceDiscountTransaction', record: this.$route.params.url}).then(() => {
+        if (this.lookup.RMA_STATUS && this.lookup.RMA_STATUS.length > 0 && this.rowData.RmaStatusId > 0) {
+          this.RmaStatus = this.lookup.RMA_STATUS.find(a => a.DecimalValue === this.rowData.RmaStatusId)
+        }
+        if (this.lookup.RETURN_TYPE && this.lookup.RETURN_TYPE.length > 0 && this.rowData.RmaTypeId > 0) {
+          this.RmaType = this.lookup.RETURN_TYPE.find(a => a.DecimalValue === this.rowData.RmaTypeId)
+        }
+      })
     }
   }
 }
 </script>
-<style lang="sass">
-</style>
