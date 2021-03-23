5be96c245f5e40bead20f4a87db4782490c6c6bd
Ahmet Sait Akyol <ahmetsaitakyol@gmail.com>
2021-03-23 22:24:47 +0300

    Statik workflow eklendi.

--- src/pages/Contracts/index.vue
+++ src/pages/Contracts/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <Nextgrid apiurl="VisionNextContractManagement/api/Contract/Search" />
+  <Nextgrid apiurl="VisionNextContractManagement/api/Contract/Search" :workFlowModel="workFlowModel" />
 </template>
+<script>
+export default {
+  data () {
+    return {
+      workFlowModel: {
+        ControllerName: '',
+        ClassName: 'Contract',
+        PageName: 'pg_Contract'
+      }
+    }
+  }
+}
+</script>
--- src/pages/KeyAccount/index.vue
+++ src/pages/KeyAccount/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Nextgrid apiurl="VisionNextCustomer/api/Customer/Search" :andConditionalModel="model" />
+  <Nextgrid apiurl="VisionNextCustomer/api/Customer/Search" :andConditionalModel="model" :workFlowModel="workFlowModel" />
 </template>
 <script>
 export default {
@@ -7,6 +7,11 @@ export default {
     return {
       model: {
         recordTypeIds: [3, 4]
+      },
+      workFlowModel: {
+        ControllerName: 'Customer',
+        ClassName: 'Customer',
+        PageName: 'pg_KeyAccount'
       }
     }
   }
