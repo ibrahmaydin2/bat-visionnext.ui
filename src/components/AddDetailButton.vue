5a7791d89b195fe52c45f239fcc1f282ac1f9504
Ahmet Sait Akyol <ahmetsaitakyol@gmail.com>
2021-04-17 02:09:30 +0300

    Multiple Cash Card eklendi

--- src/components/AddDetailButton.vue
+++ src/components/AddDetailButton.vue
@@ -1,8 +1,22 @@
 <template>
-  <b-button id="submitButton" class="mt-4" size="sm" variant="success"><i class="fa fa-plus"></i> {{$t('insert.add')}}</b-button>
+  <b-button id="submitButton" class="mt-4" size="sm" variant="success"><i class="fa fa-plus"></i> {{name}}</b-button>
 </template>
 <script>
 export default {
-  name: 'AddDetailButton'
+  name: 'AddDetailButton',
+  data () {
+    return {
+      name: null
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: null
+    }
+  },
+  mounted () {
+    this.name = this.title ? this.title : this.$t('insert.add')
+  }
 }
 </script>
