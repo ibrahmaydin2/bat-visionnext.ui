237af1e8ebf0176068e195861c4d8f8904411482
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2021-06-02 09:14:58 +0300

    version changed

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   VUE_APP_SERVICE_URL_BASE: "'https://batdev.visionnext.com.tr/'",
   VUE_APP_SYSTEM_NAME: "'BAT UI PROD'",
-  VUE_APP_VESION_NO: "'1.0.1.28'",
+  VUE_APP_VESION_NO: "'1.0.1.29'",
   BASE_URL: "'http://batfeuat.visionnext.com.tr/'"
 }
