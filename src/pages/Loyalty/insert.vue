a475e4f9e8cd5276485c47941d8a2771ca9d979f
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2021-06-15 17:55:29 +0300

    loyalty düzenleme

--- src/pages/Loyalty/insert.vue
+++ src/pages/Loyalty/insert.vue
@@ -120,7 +120,7 @@
                   <h3>{{$t('insert.loyalty.customerQuery')}}</h3>
                   <NextDetailPanel v-model="form.LoyaltyActiveCategories[index].LoyaltyActiveCtSqls" :items="loyaltyActiveCtSqlItems" />
                 </div>
-                <div class="p-4 mt-2 nested-detail-panel" v-if="(item.DatePlanType && row.item.DatePlanType.Code === 'ST') || (item.DatePlanTypeIdCode === 'ST')">
+                <div class="p-4 mt-2 nested-detail-panel" v-if="(item.DatePlanType && item.DatePlanType.Code === 'ST') || (item.DatePlanTypeIdCode === 'ST')">
                   <h3>{{$t('insert.loyalty.constantDates')}}</h3>
                   <NextDetailPanel v-model="form.LoyaltyActiveCategories[index].LoyaltyActiveCtDates" :items="loyaltyActiveCtDateItems" />
                 </div>
