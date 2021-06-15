c99e873ecc62f5573381e28bea6e8b01750ccbb2
ozcan.mengulluoglu <ozcan.mengulluoglu@ideateknoloji.com.tr>
2021-06-15 17:18:21 +0300

    branch ve analysisQuestion düzenleme

--- src/pages/AnalysisQuestions/update.vue
+++ src/pages/AnalysisQuestions/update.vue
@@ -7,7 +7,7 @@
             <Breadcrumb />
           </b-col>
           <b-col cols="12" md="6" class="text-right">
-            <router-link :to="{name: 'Dashboard' }">
+            <router-link :to="{name: 'AnalysisQuestions' }">
               <CancelButton />
             </router-link>
             <AddButton @click.native="save()" />
