4773cbf3078c0a381911e2c10c481b8caaea88be
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2021-06-21 14:41:26 +0300

    şifremi unuttum geliştirmesi

--- src/views/Auth/forgot-password.vue
+++ src/views/Auth/forgot-password.vue
@@ -1,20 +1,125 @@
 <template>
-  <div>
-    forgot-password
-  </div>
+  <b-form @submit.prevent="submitForm()">
+    <b-alert v-if="isSuccess" show variant="success">{{$t('auth.successForgotPassword')}}</b-alert>
+    <b-form-group :label="$t('auth.email')" :class="{ 'form-group--error': $v.model.userName.$error}">
+      <b-form-input v-model="model.userName" type="email" :placeholder="$t('auth.email')" />
+    </b-form-group>
+    <b-row class="text-center">
+      <b-col cols="12">
+        <b-button id="forgot-password-button" type="submit" :disabled="isLoading">
+          <span v-if="!isLoading">{{$t('auth.resetPassword')}}</span>
+          <b-spinner v-if="isLoading"/>
+        </b-button>
+      </b-col>
+      <b-col cols="12" class="my-3">
+        <router-link :to="{name: 'Login'}">{{$t('auth.login')}}</router-link>
+      </b-col>
+    </b-row>
+  </b-form>
 </template>
-
 <script>
-// import axios from 'axios'
-
+import { required, email } from 'vuelidate/lib/validators'
 export default {
   data () {
     return {
+      model: {
+        userName: '',
+        info: {
+          browser: '',
+          browserVersion: '',
+          isSecure: true,
+          userHost: '',
+          operatingSystem: ''
+        }
+      },
+      isLoading: false,
+      isSuccess: false
     }
   },
-  mounted () {
+  validations () {
+    return {
+      model: {
+        userName: {required, email}
+      }
+    }
   },
   methods: {
+    submitForm () {
+      this.$v.model.$touch()
+      if (this.$v.model.$error) {
+        this.$toasted.show(this.$t('insert.requiredFields'), {
+          type: 'error',
+          keepOnHover: true,
+          duration: '3000'
+        })
+      } else {
+        this.isLoading = true
+        let browserInfo = this.getBrowserInfo()
+        this.model.info.browser = browserInfo.browserName
+        this.model.info.browserVersion = browserInfo.fullVersion
+        this.model.info.operatingSystem = browserInfo.osName
+        this.$api.postByUrl(this.model, 'VisionNextAuthentication/api/Authentication/ForgotPassword').then((response) => {
+          this.isLoading = false
+          if (response.IsCompleted) {
+            this.isSuccess = true
+            this.model = {
+              info: {}
+            }
+            this.$v.model.$reset()
+          }
+        }).catch(() => {
+          this.isLoading = false
+          this.isSuccess = false
+        })
+      }
+    },
+    getBrowserInfo () {
+      let nAgt = navigator.userAgent
+      let browserName = navigator.appName
+      let fullVersion = '' + parseFloat(navigator.appVersion)
+      let nameOffset, verOffset, ix
+      let osName = ''
+      if ((verOffset = nAgt.indexOf('Opera')) !== -1) {
+        browserName = 'Opera'
+        fullVersion = nAgt.substring(verOffset + 6)
+        if ((verOffset = nAgt.indexOf('Version')) !== -1) {
+          fullVersion = nAgt.substring(verOffset + 8)
+        }
+      } else if ((verOffset = nAgt.indexOf('MSIE')) !== -1) {
+        browserName = 'Microsoft Internet Explorer'
+        fullVersion = nAgt.substring(verOffset + 5)
+      } else if ((verOffset = nAgt.indexOf('Chrome')) !== -1) {
+        browserName = 'Chrome'
+        fullVersion = nAgt.substring(verOffset + 7)
+      } else if ((verOffset = nAgt.indexOf('Safari')) !== -1) {
+        browserName = 'Safari'
+        fullVersion = nAgt.substring(verOffset + 7)
+        if ((verOffset = nAgt.indexOf('Version')) !== -1) {
+          fullVersion = nAgt.substring(verOffset + 8)
+        }
+      } else if ((verOffset = nAgt.indexOf('Firefox')) !== -1) {
+        browserName = 'Firefox'
+        fullVersion = nAgt.substring(verOffset + 8)
+      } else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
+        browserName = nAgt.substring(nameOffset, verOffset)
+        fullVersion = nAgt.substring(verOffset + 1)
+        if (browserName.toLowerCase() === browserName.toUpperCase()) {
+          browserName = navigator.appName
+        }
+      }
+
+      if ((ix = fullVersion.indexOf(';')) !== -1) {
+        fullVersion = fullVersion.substring(0, ix)
+      }
+      if ((ix = fullVersion.indexOf(' ')) !== -1) {
+        fullVersion = fullVersion.substring(0, ix)
+      }
+      return {
+        browserName: browserName,
+        fullVersion: fullVersion,
+        osName: osName
+      }
+    }
   }
 }
 </script>
