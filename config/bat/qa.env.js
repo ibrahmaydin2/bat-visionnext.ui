f34fac08aa3760cd2773709314a38cb34a4e0004
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2021-06-24 10:16:54 +0300

    version changed

--- config/bat/qa.env.js
+++ config/bat/qa.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"qa"',
   VUE_APP_SERVICE_URL_BASE: "'https://batdev.visionnext.com.tr/'",
   VUE_APP_SYSTEM_NAME: "'BAT UI QA'",
-  VUE_APP_VESION_NO: "'1.0.1.33'",
+  VUE_APP_VESION_NO: "'1.0.1.34'",
   BASE_URL: "'http://batfedev.visionnext.com.tr/'",
   TENANT: "'BAT'"
 })
