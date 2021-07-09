aba3ef0cae263ab94a9fbaea98c53970af15de86
ozcan.mengulluoglu <ozcan.mengulluoglu@ideateknoloji.com.tr>
2021-07-09 15:42:13 +0300

    bcp and ıntegrationGet page

---
+++ src/pages/Bcp/insert.vue
@@ -0,0 +1,117 @@
+<template>
+  <b-row>
+    <b-col cols="12">
+      <header>
+        <b-row>
+          <b-col cols="12" md="6">
+            <Breadcrumb />
+          </b-col>
+          <b-col cols="12" md="6" class="text-right">
+            <router-link :to="{name: 'Bcp' }">
+              <CancelButton />
+            </router-link>
+            <AddButton @click.native="save()" />
+          </b-col>
+        </b-row>
+      </header>
+    </b-col>
+    <b-col cols="12" class="asc__insertPage-content-head">
+      <section>
+        <b-row>
+          <NextFormGroup item-key="Code" :error="$v.form.Code">
+            <NextInput v-model="form.Code" type="text" :disabled="insertReadonly.Code" />
+          </NextFormGroup>
+          <NextFormGroup item-key="StatusId" :error="$v.form.StatusId">
+            <NextCheckBox v-model="form.StatusId" type="number" :disabled="insertReadonly.StatusId" toggle/>
+          </NextFormGroup>
+          <NextFormGroup item-key="Description1" :error="$v.form.Description1">
+            <NextInput type="text" v-model="form.Description1" :disabled="insertReadonly.Description1"/>
+          </NextFormGroup>
+        </b-row>
+      </section>
+    </b-col>
+    <b-col cols="12">
+      <b-tabs>
+        <b-tab :title="$t('insert.bcp.Bcp')" active>
+          <b-row>
+            <NextFormGroup item-key="DiscountGroup7Id" :error="$v.form.DiscountGroup7Id">
+              <NextDropdown :disabled="insertReadonly.DiscountGroup7Id" url="" @input="selectedSearchType('DiscountGroup7Id', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="Year" :error="$v.form.Year">
+              <NextDropdown :disabled="insertReadonly.Year" url="" @input="selectedSearchType('Year', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="Month" :error="$v.form.Month">
+              <NextDropdown :disabled="insertReadonly.Month" url="" @input="selectedSearchType('Month', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="EndYear" :error="$v.form.EndYear">
+              <NextDropdown :disabled="insertReadonly.EndYear" url="" @input="selectedSearchType('EndYear', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="EndMonth" :error="$v.form.EndMonth">
+              <NextDropdown :disabled="insertReadonly.EndMonth" url="" @input="selectedSearchType('EndMonth', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="CustomerRegion3Id" :error="$v.form.CustomerRegion3Id">
+              <NextDropdown :disabled="insertReadonly.CustomerRegion3Id" url="" @input="selectedSearchType('CustomerRegion3Id', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="BranchCriteriaId" :error="$v.form.BranchCriteriaId">
+              <NextDropdown :disabled="insertReadonly.BranchCriteriaId" lookup-key="" @input="selectedSearchType('BranchCriteriaId', $event)"/>
+            </NextFormGroup>
+          </b-row>
+        </b-tab>
+        <b-tab :title="$t('insert.bcp.items')">
+          <NextDetailPanel v-model="form.BcpDetails" :items="bcpDetailsItems"/>
+        </b-tab>
+        <b-tab :title="$t('insert.bcp.branchs')">
+          <NextDetailPanel v-model="form.BcpBranchs" :items="bcpBranchsItems"/>
+        </b-tab>
+      </b-tabs>
+    </b-col>
+  </b-row>
+</template>
+<script>
+import mixin from '../../mixins/insert'
+import { detailData } from './detailPanelData'
+export default {
+  mixins: [mixin],
+  data () {
+    return {
+      form: {
+        Deleted: 0,
+        System: 0,
+        StatusId: 1,
+        RecordState: 2,
+        Code: null,
+        Description1: null,
+        DiscountGroup7Id: null,
+        Year: null,
+        Month: null,
+        EndYear: null,
+        EndMonth: null,
+        CustomerRegion3Id: null,
+        BranchCriteriaId: null,
+        BcpDetails: [],
+        BcpBranchs: []
+      },
+      routeName1: 'Customer',
+      bcpDetailsItems: detailData.bcpDetailsItems,
+      bcpBranchsItems: detailData.bcpBranchsItems
+    }
+  },
+  mounted () {
+    this.createManualCode()
+  },
+  methods: {
+    save () {
+      this.$v.form.$touch()
+      if (this.$v.form.$error) {
+        this.$toasted.show(this.$t('insert.requiredFields'), {
+          type: 'error',
+          keepOnHover: true,
+          duration: '3000'
+        })
+      } else {
+        this.createData()
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/Bcp/update.vue
@@ -0,0 +1,144 @@
+<template>
+  <b-row>
+    <b-col cols="12">
+      <header>
+        <b-row>
+          <b-col cols="12" md="6">
+            <Breadcrumb />
+          </b-col>
+          <b-col cols="12" md="6" class="text-right">
+            <router-link :to="{name: 'Bcp' }">
+              <CancelButton />
+            </router-link>
+            <AddButton @click.native="save()" />
+          </b-col>
+        </b-row>
+      </header>
+    </b-col>
+    <b-col cols="12" class="asc__insertPage-content-head">
+      <section>
+        <b-row>
+          <NextFormGroup item-key="Code" :error="$v.form.Code">
+            <NextInput v-model="form.Code" type="text" :disabled="insertReadonly.Code" />
+          </NextFormGroup>
+          <NextFormGroup item-key="StatusId" :error="$v.form.StatusId">
+            <NextCheckBox v-model="form.StatusId" type="number" :disabled="insertReadonly.StatusId" toggle/>
+          </NextFormGroup>
+          <NextFormGroup item-key="Description1" :error="$v.form.Description1">
+            <NextInput type="text" v-model="form.Description1" :disabled="insertReadonly.Description1"/>
+          </NextFormGroup>
+        </b-row>
+      </section>
+    </b-col>
+    <b-col cols="12">
+      <b-tabs>
+        <b-tab :title="$t('insert.bcp.Bcp')" active>
+          <b-row>
+            <NextFormGroup item-key="DiscountGroup7Id" :error="$v.form.DiscountGroup7Id">
+              <NextDropdown v-model="DiscountGroup7" :disabled="insertReadonly.DiscountGroup7Id" url="" @input="selectedSearchType('DiscountGroup7Id', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="Year" :error="$v.form.Year">
+              <NextDropdown v-model="year" :disabled="insertReadonly.Year" url="" @input="selectedSearchType('Year', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="Month" :error="$v.form.Month">
+              <NextDropdown v-model="month" :disabled="insertReadonly.Month" url="" @input="selectedSearchType('Month', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="EndYear" :error="$v.form.EndYear">
+              <NextDropdown v-model="endyear" :disabled="insertReadonly.EndYear" url="" @input="selectedSearchType('EndYear', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="EndMonth" :error="$v.form.EndMonth">
+              <NextDropdown v-model="endMonth" :disabled="insertReadonly.EndMonth" url="" @input="selectedSearchType('EndMonth', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="CustomerRegion3Id" :error="$v.form.CustomerRegion3Id">
+              <NextDropdown v-model="CustomerRegion3" :disabled="insertReadonly.CustomerRegion3Id" url="" @input="selectedSearchType('CustomerRegion3Id', $event)"/>
+            </NextFormGroup>
+            <NextFormGroup item-key="BranchCriteriaId" :error="$v.form.BranchCriteriaId">
+              <NextDropdown v-model="BranchCriteria" :disabled="insertReadonly.BranchCriteriaId" lookup-key="" @input="selectedSearchType('BranchCriteriaId', $event)"/>
+            </NextFormGroup>
+          </b-row>
+        </b-tab>
+        <b-tab :title="$t('insert.bcp.items')">
+          <NextDetailPanel v-model="form.BcpDetails" :items="bcpDetailsItems"/>
+        </b-tab>
+        <b-tab :title="$t('insert.bcp.branchs')">
+          <NextDetailPanel v-model="form.BcpBranchs" :items="bcpBranchsItems"/>
+        </b-tab>
+      </b-tabs>
+    </b-col>
+  </b-row>
+</template>
+<script>
+import { mapState } from 'vuex'
+import updateMixin from '../../mixins/update'
+import { detailData } from './detailPanelData'
+export default {
+  mixins: [updateMixin],
+  data () {
+    return {
+      form: {
+        Deleted: 0,
+        System: 0,
+        StatusId: 1,
+        RecordState: 2,
+        Code: null,
+        Description1: null,
+        DiscountGroup7Id: null,
+        Year: null,
+        Month: null,
+        EndYear: null,
+        EndMonth: null,
+        CustomerRegion3Id: null,
+        BranchCriteriaId: null,
+        BcpDetails: [],
+        BcpBranchs: []
+      },
+      routeName1: 'Customer',
+      DiscountGroup7: {},
+      year: {},
+      month: {},
+      endyear: {},
+      endMonth: {},
+      CustomerRegion3: {},
+      BranchCriteria: {},
+      bcpDetailsItems: detailData.bcpDetailsItems,
+      bcpBranchsItems: detailData.bcpBranchsItems
+    }
+  },
+  computed: {
+    ...mapState([''])
+  },
+  mounted () {
+    this.getData().then(() => this.setData())
+  },
+  methods: {
+    setData () {
+      let rowData = this.rowData
+      this.form = rowData
+      if (!rowData.BcpDetails) {
+        this.form.BcpDetails = []
+      }
+      if (!rowData.BcpBranchs) {
+        this.form.BcpBranchs = []
+      }
+    },
+    save () {
+      this.$v.form.$touch()
+      if (this.$v.form.$error) {
+        this.$toasted.show(this.$t('insert.requiredFields'), {
+          type: 'error',
+          keepOnHover: true,
+          duration: '3000'
+        })
+        this.tabValidation()
+      } else {
+        this.updateData()
+      }
+    }
+  },
+  validations () {
+    return {
+      form: this.insertRules
+    }
+  }
+}
+</script>
