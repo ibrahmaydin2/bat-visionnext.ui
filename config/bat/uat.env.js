5acf8652f4afbb4063c9090f4571595e8f2119ce
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2021-08-19 13:49:28 +0300

    versiyon update

--- config/bat/uat.env.js
+++ config/bat/uat.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"uat"',
   VUE_APP_SERVICE_URL_BASE: "'https://battestvnapi.visionplus.com.tr/'",
   VUE_APP_SYSTEM_NAME: "'BAT UI UAT'",
-  VUE_APP_VESION_NO: "'1.0.1.37'",
+  VUE_APP_VESION_NO: "'1.0.1.38'",
   BASE_URL: "'http://batfeuat.visionnext.com.tr/'",
   TENANT: "'BAT'",
   HASH: "'8ced6612-e737-4f10-82be-76db0177e462'"
