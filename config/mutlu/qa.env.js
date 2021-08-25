4ee5ec37174a1a8f42818e53e0286c9a580dc7ba
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2021-08-25 13:03:18 +0300

    versiyon update

--- config/mutlu/qa.env.js
+++ config/mutlu/qa.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"qa"',
   VUE_APP_SERVICE_URL_BASE: "'https://mutluvnapiuat.visionnext.com.tr/'",
   VUE_APP_SYSTEM_NAME: "'MUTLU UI DEVELOPMENT'",
-  VUE_APP_VESION_NO: "'1.0.1.38'",
+  VUE_APP_VESION_NO: "'1.0.1.39'",
   BASE_URL: "'https://mutluat.visionnext.com.tr/'",
   TENANT: "'MUTLU'",
   HASH: "'8ced6612-e737-4f10-82be-76db0177e462'"
