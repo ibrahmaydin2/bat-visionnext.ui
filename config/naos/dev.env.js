02d0b24ddfad1d1dc67abd1496109aad0ebc319e
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2021-09-13 17:04:35 +0300

    config düzenlemeler

--- config/naos/dev.env.js
+++ config/naos/dev.env.js
@@ -4,11 +4,11 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_SERVICE_URL_BASE: "'https://batdev.visionnext.com.tr/'",
-  VUE_APP_SYSTEM_NAME: "'BAT UI DEVELOPMENT'",
+  VUE_APP_SERVICE_URL_BASE: "'https://naosapiuat.visionnext.com.tr/'",
+  VUE_APP_SYSTEM_NAME: "'NAOS UI DEVELOPMENT'",
   VUE_APP_VESION_NO: "'1.0.1.42'",
-  BASE_URL: "'http://batfedev.visionnext.com.tr/'",
-  TENANT: "'BAT'",
-  SITE_NAME: "'BAT'",
+  BASE_URL: "'https://naosdev.visionnext.com.tr/'",
+  TENANT: "'SAAS'",
+  SITE_NAME: "'NAOS'",
   HASH: "'8ced6612-e737-4f10-82be-76db0177e462'"
 })
---
+++ config/naos/prod.env.js
@@ -0,0 +1,11 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  VUE_APP_SERVICE_URL_BASE: "'https://naosapiuat.visionnext.com.tr/'",
+  VUE_APP_SYSTEM_NAME: "'NAOS UI PRODUCTION'",
+  VUE_APP_VESION_NO: "'1.0.1.42'",
+  BASE_URL: "'https://naos.visionnext.com.tr/'",
+  TENANT: "'SAAS'",
+  SITE_NAME: "'NAOS'",
+  HASH: "'8ced6612-e737-4f10-82be-76db0177e462'"
+}
