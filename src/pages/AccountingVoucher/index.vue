05d1905224d03aefba5f401ccb65379705812ee1
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2021-12-20 17:59:37 +0300

    gereksiz sayfaların silinmesi ve ekran yenilenmesi

--- src/pages/AccountingVoucher/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <Nextgrid apiurl="VisionNextInventoryTransaction/api/InventoryTransaction/Search" />
-</template>
--- src/pages/TerminalPasswordLog/get.vue
+++ src/pages/TerminalPasswordLog/get.vue
@@ -3,7 +3,7 @@
     <div class="asc__showPage-times">
       <i class="fas fa-times-circle" @click="closeQuick()" />
     </div>
-    <div v-if="rowData" class="asc__showPage-container">
+    <div class="asc__showPage-container">
       <b-row>
         <b-col cols="12">
           <header>
@@ -17,22 +17,21 @@
           <section>
             <span><i class="fas fa-code" />  <b>{{$t('get.code')}}:</b> {{rowData.Code }}</span>
             <span><i class="fas fa-code" />  <b>{{$t('get.description1')}}:</b> {{rowData.Description1 }}</span>
-            <span><i class="fas fa-check" />  <b>{{$t('get.status')}}:</b> {{rowData.StatusId ? $t('insert.active') : $t('insert.passive') }}</span>
+            <span><i class="fas fa-check" />  <b>{{$t('get.status')}}:</b> {{rowData.StatusId === 1 ? $t('insert.active') : $t('insert.passive') }}</span>
           </section>
         </b-col>
       </b-row>
       <b-tabs>
         <b-tab :title="$t('get.detail')" active>
-          <b-row>
-            <b-col cols="12" md="4">
-              <b-card class="m-3 asc__showPage-card">
-                <h6>{{$t('get.detail')}}</h6>
-                <span><i class="far fa-circle" /> {{$t('get.terminal.Employee')}}</span> <p>{{rowData.Employee ? rowData.Employee.Label : ''}}</p>
-                <span><i class="far fa-circle" /> {{$t('get.terminal.Keyword')}}</span> <p>{{rowData.Keyword}}</p>
-                <span><i class="far fa-circle" /> {{$t('get.terminal.Password')}}</span> <p>{{rowData.Password}}</p>
-                <span><i class="far fa-circle" /> {{$t('get.terminal.OperationDate')}}</span> <p>{{rowData.OperationDate ? dateConvertFromTimezone(rowData.OperationDate) : ''}}</p>
-              </b-card>
-            </b-col>
+          <b-row class="p-4">
+            <b-card class="col-md-6 col-12 asc__showPage-card">
+              <div v-html="getFormatDataByType(rowData.Employee, 'object', 'get.terminal.Employee')"></div>
+              <div v-html="getFormatDataByType(rowData.Keyword, 'text', 'get.terminal.Keyword')"></div>
+            </b-card>
+            <b-card class="col-md-6 col-12 asc__showPage-card">
+              <div v-html="getFormatDataByType(rowData.Password, 'text', 'get.terminal.Password')"></div>
+              <div v-html="getFormatDataByType(rowData.OperationDate, 'date', 'get.terminal.OperationDate')"></div>
+            </b-card>
           </b-row>
         </b-tab>
       </b-tabs>
@@ -44,16 +43,11 @@ import { mapState } from 'vuex'
 import mixin from '../../mixins/index'
 export default {
   mixins: [mixin],
-  props: ['dataKey'],
-  data () {
-    return {
-    }
-  },
   mounted () {
     this.getData()
   },
   computed: {
-    ...mapState(['rowData', 'style'])
+    ...mapState(['rowData'])
   },
   methods: {
     closeQuick () {
@@ -65,5 +59,3 @@ export default {
   }
 }
 </script>
-<style lang="sass">
-</style>
