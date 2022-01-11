702d3fedc4c5623e137f3777aae34a8d8e14715c
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2022-01-11 16:45:10 +0300

    rma bugfix

--- src/pages/Rma/insert.vue
+++ src/pages/Rma/insert.vue
@@ -39,8 +39,9 @@
               <NextDropdown
                 @input="selectedSearchType('WarehouseId', $event)"
                 url="VisionNextWarehouse/api/Warehouse/AutoCompleteSearch"
+                :dynamic-and-condition="{ StatusIds: [1], IsVehicle: 0 }"
                 label="Description1"
-                :disabled="insertReadonly.WarehouseId" searchable/>
+                :disabled="insertReadonly.WarehouseId"/>
             </NextFormGroup>
             <NextFormGroup item-key="RepresentativeId" :error="$v.form.RepresentativeId">
               <NextDropdown
@@ -65,7 +66,6 @@
                 :disabled="insertReadonly.ApproveEmployeeId"
                 url="VisionNextEmployee/api/Employee/AutoCompleteSearch"
                 orConditionFields="Code,Description1,Name,Surname"
-                v-model="approveEmployeeName"
                 label="Description1"
                 />
             </NextFormGroup>
@@ -124,17 +124,8 @@
             <NextFormGroup :title="$t('insert.RMA.lastSalesQuantity')" md="2" lg="2">
               <NextInput type="text" v-model="rmaLine.Item.LastSalesQuantity" :disabled="true"/>
             </NextFormGroup>
-             <NextFormGroup :title="$t('insert.RMA.lastSalesPrice')" md="2" lg="2">
-              <NextInput type="text" v-model="rmaLine.Item.LastPrice" :disabled="true"/>
-            </NextFormGroup>
             <NextFormGroup :title="$t('insert.RMA.Quantity')" :required="true" :error="$v.rmaLine.Quantity" md="2" lg="2">
               <NextInput type="number" v-model="rmaLine.Quantity" @keypress="onlyForCurrencyDot($event)" min="1" />
-            </NextFormGroup>
-            <NextFormGroup :title="$t('insert.RMA.meanPrice')" md="2" lg="2">
-              <NextInput type="text" v-model="rmaLine.Item.MeanPrice" :disabled="true"/>
-            </NextFormGroup>
-            <NextFormGroup :title="$t('insert.RMA.listPrice')" md="2" lg="2">
-              <NextInput type="text" v-model="rmaLine.Item.ListPrice" :disabled="true"/>
             </NextFormGroup>
             <NextFormGroup :title="$t('insert.RMA.usedPrice')" md="2" lg="2">
               <NextInput type="text" v-model="rmaLine.Item.UsedPrice" :disabled="true"/>
@@ -162,10 +153,8 @@
                   <b-th><span>{{$t('insert.RMA.Item')}}</span></b-th>
                   <b-th><span>{{$t('insert.RMA.ItemName')}}</span></b-th>
                   <b-th><span>{{$t('insert.RMA.lastSalesQuantity')}}</span></b-th>
-                  <b-th><span>{{$t('insert.RMA.lastSalesPrice')}}</span></b-th>
                   <b-th><span>{{$t('insert.RMA.Quantity')}}</span></b-th>
-                  <b-th><span>{{$t('insert.RMA.meanPrice')}}</span></b-th>
-                  <b-th><span>{{$t('insert.RMA.listPrice')}}</span></b-th>
+                  <b-th><span>{{$t('insert.RMA.UsedQuantity')}}</span></b-th>
                   <b-th><span>{{$t('insert.RMA.usedPrice')}}</span></b-th>
                   <b-th><span>{{$t('list.operations')}}</span></b-th>
                 </b-thead>
@@ -174,11 +163,9 @@
                     <b-td>{{w.Item ? w.Item.Code : w.Code}}</b-td>
                     <b-td>{{w.Item ? w.Item.Description1 : w.Description1}}</b-td>
                     <b-td>{{w.Item ? w.Item.LastSalesQuantity : w.LastSalesQuantity}}</b-td>
-                    <b-td>{{w.Item ? w.Item.LastPrice :  w.LastPrice}}</b-td>
                     <b-td>{{w.Quantity}}</b-td>
-                    <b-td>{{w.Item ? w.Item.MeanPrice : w.MeanPrice}}</b-td>
-                    <b-td>{{w.Item ? w.Item.ListPrice : w.ListPrice}}</b-td>
-                    <b-td>{{w.Item ? w.Item.UsedPrice : w.UsedPrice}}</b-td>
+                    <b-td>{{w.Item ? w.Item.UsedQuantity :  w.UsedQuantity}}</b-td>
+                    <b-td>{{getPrice(w)}}</b-td>
                     <b-td class="text-center">
                       <b-button :title="$t('list.delete')" @click="removeItems(w)" class="btn mr-2 btn-danger btn-sm">
                         <i class="far fa-trash-alt"></i>
@@ -242,7 +229,6 @@ export default {
         Item: {}
       },
       rmaLines: [],
-      approveEmployeeName: null,
       representativeName: null,
       routeName1: 'Rma'
     }
@@ -265,8 +251,6 @@ export default {
         this.$api.postByUrl(request, 'VisionNextSystem/api/SysUser/Search').then(response => {
           if (response && response.ListModel && response.ListModel.BaseModels && response.ListModel.BaseModels.length > 0) {
             let user = response.ListModel.BaseModels[0]
-            this.approveEmployeeName = `${userModel.Name} ${userModel.Surname}`
-            this.form.ApproveEmployeeId = user.EmployeeId
             this.representativeName = `${userModel.Name} ${userModel.Surname}`
             this.form.RepresentativeId = user.EmployeeId
           }
@@ -310,6 +294,8 @@ export default {
       this.rmaLine.UnitId = value.UnitId
       this.rmaLine.RmaQuantity1 = this.rmaLine.Quantity
       this.rmaLine.RmaUnit1Id = this.rmaLine.UnitId
+      this.rmaLine.Code = value.Code
+      this.rmaLine.Description1 = value.Description1
     },
     initNullRmaLine () {
       this.rmaLine = {
@@ -366,6 +352,19 @@ export default {
           return r
         })
         this.createData()
+      }
+    },
+    getPrice (data) {
+      if (data.item) {
+        if (data.item.Price) {
+          return data.item.Price
+        } else {
+          return data.item.UsedPrice
+        }
+      } else if (data.Price) {
+        return data.Price
+      } else {
+        return data.UsedPrice
       }
     }
   },
