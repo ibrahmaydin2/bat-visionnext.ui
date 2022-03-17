3d35e70de13a1d28dffaf2d7c0b064d40a74394f
Tolga Coşkun <tolga.coskun@ideateknoloji.com.tr>
2022-03-17 12:48:31 +0300

    new component added

---
+++ src/components/NextExportDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-button class="mt-4" size="sm" variant="success"><i class="fas fa-file-excel"></i> {{name}}</b-button>
+</template>
+<script>
+export default {
+  name: 'AddDetailButton',
+  data () {
+    return {
+      name: null
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: null,
+      description: 'Buton title bilgisi'
+    }
+  },
+  mounted () {
+    this.name = this.title ? this.title : this.$t('insert.add')
+  }
+}
+</script>
