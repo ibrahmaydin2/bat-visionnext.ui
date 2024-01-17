d179eb2535b023dd3d9adb60049f67378c45a2c8
Eren <eren.dumlupinar@ideateknoloji.com.tr>
2024-01-17 11:58:23 +0300

    BATVNW-215

---
+++ src/components/NextCheckBoxSalesMaximumQty.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <b-form-checkbox :class="inputClass" v-if="inputType === 'Boolean'" v-model="model" name="check-button" :switch="toggle" :disabled="disabled" @input="input($event)">
+      <span v-if="showText">{{model ? textActive : textPassive}}</span>
+    </b-form-checkbox>
+    <b-form-radio-group
+        v-if="inputType === 'Radio'"
+        v-model="model"
+        :options="options"
+        :disabled="disabled"
+        @input="input($event)"
+      ></b-form-radio-group>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NextCheckBox',
+  props: {
+    type: {
+      type: String,
+      validator: (prop) => [
+        'boolean',
+        'number'
+      ].includes(prop),
+      default: 'boolean',
+      description: 'Modelin number veya boolean olarak geri dönüşünü sağlar'
+    },
+    activeText: {
+      type: String,
+      default: '',
+      description: 'Checkbox seçili olduğunda çıkan yazı bilgisi'
+    },
+    passiveText: {
+      type: String,
+      default: '',
+      description: 'Checkbox seçili olmadığında çıkan yazı bilgisi'
+    },
+    value: {
+      type: Number | Boolean,
+      description: 'v-model bilgisi'
+    },
+    toggle: {
+      type: Boolean,
+      default: false,
+      description: 'Toggle şeklinde görünüp görünmeme bilgisi'
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+      description: 'Disabled bilgisi'
+    },
+    showText: {
+      type: Boolean,
+      default: true,
+      description: 'Checkboxın abel bilgisinin görünürlüğü bilgisi'
+    },
+    radio: {
+      type: Boolean,
+      default: false,
+      description: 'radio button şeklinde görünüp görünmeme bilgisi'
+    },
+    inputClass: {
+      type: String,
+      default: '',
+      description: 'Checkbox inputunun css class bilgisi'
+    }
+  },
+  model: {
+    prop: 'value',
+    event: 'valuechange'
+  },
+  data () {
+    return {
+      textActive: '',
+      textPassive: '',
+      model: false,
+      selectedValue: null,
+      options: [],
+      inputType: 'Boolean'
+    }
+  },
+  mounted () {
+    if (this.$parent && this.$parent.$parent && this.$parent.$parent.itemKey) {
+      let itemKey = this.$parent.$parent.itemKey
+      let insertColumnType = this.$store.state.insertColumnType
+      if (insertColumnType && insertColumnType[itemKey]) {
+        this.inputType = insertColumnType[itemKey]
+      }
+    }
+    if (this.radio) {
+      this.inputType = 'Radio'
+    }
+    this.textActive = this.activeText ? this.activeText : 1
+    this.textPassive = this.passiveText ? this.passiveText : 0
+    this.options = [
+      { text: this.$t('insert.yes'), value: 1 },
+      { text: this.$t('insert.no'), value: 0 }
+    ]
+    if (this.inputType === 'Boolean') {
+      this.model = this.type === 'boolean' ? this.value : this.value === 1 || this.value === true
+      if (this.type === 'number') {
+        this.selectedValue = this.value === true || this.value === 1 ? 1 : 0
+        this.$emit('valuechange', this.selectedValue)
+      }
+    } else {
+      this.model = this.value
+    }
+  },
+  watch: {
+    model (newValue) {
+      let val = null
+      if (this.inputType === 'Boolean') {
+        val = this.type === 'boolean' ? newValue : newValue ? 1 : 0
+      } else {
+        val = newValue
+      }
+      if (val !== this.value) {
+        this.$emit('valuechange', val)
+      }
+    },
+    value (newValue) {
+      let val = null
+      if (this.inputType === 'Boolean') {
+        val = this.type === 'boolean' ? newValue : newValue === 1 || this.value === true
+      } else {
+        val = newValue
+      }
+      if (val !== this.model) {
+        this.model = val
+      }
+    }
+  },
+  methods: {
+    input (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+
+</script>
--- src/pages/SalesMaximumQuantity/get.vue
+++ src/pages/SalesMaximumQuantity/get.vue
@@ -28,9 +28,26 @@
         <b-tab :title="$t('insert.SalesMaximumQuantity.CustomerList')">
           <b-row>
             <b-col>
-              <b-card class="m-3 asc__showPage-card">
-                <NextDetailPanel type="get" v-model="rowData.SalesMaximumQuantityCustomers" :items="salesMaximumQuantityCustomersItems" />
-              </b-card>
+              <!-- <b-card class="m-3 asc__showPage-card">
+                <NextDetailPanelSalesMaxQuantity type="get" v-model="rowData.SalesMaximumQuantityCustomers" :items="salesMaximumQuantityCustomersItems" />
+              </b-card> -->
+                <b-card class="m-3 asc__showPage-card">
+                  <b-table
+                    :items="rowData.SalesMaximumQuantityCustomers"
+                    :fields="fields"
+                    :current-page="SalesMaximumQuantityCustomersCurrentPage"
+                    :per-page="perPage"
+                    small
+                    bordered
+                    responsive
+                  >
+                  </b-table>
+                  <b-pagination
+                    v-model="SalesMaximumQuantityCustomersCurrentPage"
+                    :total-rows="rowData.SalesMaximumQuantityCustomers.length"
+                    :per-page="perPage">
+                  </b-pagination>
+                </b-card>
             </b-col>
           </b-row>
         </b-tab>
@@ -41,13 +58,22 @@
 <script>
 import { mapState } from 'vuex'
 import mixin from '../../mixins/index'
-import { detailData } from './detailPanelData'
+//import { detailData } from './detailPanelData'
 export default {
   props: ['dataKey'],
   mixins: [mixin],
   data () {
     return {
-      salesMaximumQuantityCustomersItems: detailData.salesMaximumQuantityCustomersItems
+      //salesMaximumQuantityCustomersItems: detailData.salesMaximumQuantityCustomersItems,
+      SalesMaximumQuantityCustomersCurrentPage : 1,
+      perPage: 10,
+      fields: [
+        { key: 'Code', label: 'Kod' },
+        { key: 'Customer.Code', label: 'Müşteri Kodu' },
+        { key: 'Customer.Label', label: 'Müşteri Adı' },
+        { key: 'IsTotalQuantity', label: 'Üst Limitleme Yöntemi' },
+        { key: 'Quantity', label: 'Miktar' },
+      ],
     }
   },
   mounted () {
